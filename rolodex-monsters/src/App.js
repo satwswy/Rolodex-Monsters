56cad22f13707d9434693d18ba66f833b01fa632
marios <mgeorgak859@gmail.com>
2022-08-31 06:37:18 +0300

    added components folder

--- rolodex-monsters/src/App.js
+++ rolodex-monsters/src/App.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import logo from "./logo.svg";
+import CardList from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -51,13 +51,14 @@ class App extends Component {
           placeholder="search monsters"
           onChange={onSearchChange}
         />
-        {filteredMonsters.map((monster) => {
+        {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })}
+        })} */}
+        <CardList />
       </div>
     );
   }
