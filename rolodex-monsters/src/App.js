50b781010797e30147263f772ba8d0e3043dabfa
marios <mgeorgak859@gmail.com>
2022-09-18 21:01:05 +0300

    Update App.js

--- rolodex-monsters/src/App.js
+++ rolodex-monsters/src/App.js
@@ -3,6 +3,23 @@ import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
+
+// const App = () => {
+
+
+//   return(
+//     <div className="App">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+//         <SearchBox
+//          onChangeHandler={onSearchChange} 
+//          placeholder='search monsters'
+//          className='monsters-search-box'
+//          />
+//         <CardList monsters={filteredMonsters}/>
+//       </div>
+//   )
+// }
+
 class App extends Component {
   constructor() {
     super();
